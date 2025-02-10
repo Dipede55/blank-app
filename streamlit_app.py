8238e3d896128a3799a8178733d0561143bd49c0
Dipede55 <128988529+Dipede55@users.noreply.github.com>
2025-02-10 14:35:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,33 @@
 import streamlit as st
 
 st.title("🎈 My new app")
+import streamlit_app as st
+import pandas as pd 
+import plotly.express as pd
+import os
+print(os.path.exists("style.css"))  # Should print True if the file exists
+
+st.set_page_config(layout= 'wide', initial_sidebar_state= 'expanded') 
+#Expands the app layout to use the full width of the screen
+#Expands the sidebar by default when the app loads.
+with open("style.css") as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html= True)
+st.sidebar.header('Menu')
+st.sidebar.subheader('Money income')
+
+st.title("♉ My fianace dashboard ☻")
+st.write()
+#input income
+income = st.number_input("Income (thousand): ", min_value = 0, value = 7000, step = 1)
+
+# Expense Goods
+goods = ["Rent", "Merch", "Debt", "Foods", "Drinks", "Petrol", "Shopping", "Others"]
+expense_data = {}
+for good in goods:
+    expense_data[good] = st.number_input(f"{goods} expense (thousand)", min_value = 0, value = 0, step = 10)
+
+
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
